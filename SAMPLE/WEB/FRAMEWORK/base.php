cd4c4e1b7eaee916af95ad8ee4d1eceabf56f76d
senselogic <ecstatic.coder@gmail.com>
2020-04-03 20:36:55 +0200

    % base

--- SAMPLE/WEB/FRAMEWORK/base.php
+++ SAMPLE/WEB/FRAMEWORK/base.php
@@ -1,5 +1,69 @@
 <?php // -- FUNCTIONS
 
+function HasGlobalValue(
+    string $name
+    )
+{
+    return isset( $GLOBALS[ $name ] );
+}
+
+// ~~
+
+function GetGlobalValue(
+    string $name
+    )
+{
+    return $GLOBALS[ $name ];
+}
+
+// ~~
+
+function FindGlobalValue(
+    string $name,
+    $default_value
+    )
+{
+    if ( isset( $GLOBALS[ $name ] ) )
+    {
+        return $GLOBALS[ $name ];
+    }
+    else
+    {
+        return $default_value;
+    }
+}
+
+// ~~
+
+function SetGlobalValue(
+    string $name,
+    $value
+    )
+{
+    $GLOBALS[ $name ] = $value;
+}
+
+// ~~
+
+function SetGlobalValueOnce(
+    string $name,
+    $value
+    )
+{
+    if ( !isset( $GLOBALS[ $name ] ) )
+    {
+        $GLOBALS[ $name ] = $value;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// ~~
+
 function GetJsonText(
     $value
     )
